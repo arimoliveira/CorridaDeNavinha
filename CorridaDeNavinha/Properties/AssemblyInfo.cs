c08a950b5a29e16217483c0cf8e905c085d1f73d
Fernando Pazin <pazinfernando@gmail.com>
2011-05-08 17:17:36 -0700

    Commit com o esqueleto do projeto

---
+++ CorridaDeNavinha/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CorridaDeNavinha")]
+[assembly: AssemblyProduct("CorridaDeNavinha")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("FP")]
+
+[assembly: AssemblyCopyright("Copyright © FP 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5a3b4278-def9-4b52-90a0-fcade06c7f64")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
